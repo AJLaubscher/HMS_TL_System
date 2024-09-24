5e3476623c6cfd634858b1f5a949a6c11fcccf33
AJLaubscher <141874531+AJLaubscher@users.noreply.github.com>
2024-09-24 14:58:49 +0200

    added error handling and required fields for creating and updating records

--- HMS_API/Dtos/module/CreateModuleDto.cs
+++ HMS_API/Dtos/module/CreateModuleDto.cs
@@ -4,8 +4,8 @@ namespace HMS_API.Dtos.module;
 public record class CreateModuleDto //comments indicates database default values/ inserted by each creation
 (
 	//Id
-	[Required] string Code,
-	[Required] string ModName,
+	[Required] [StringLength(7)] string Code,
+	[Required] [StringLength(20)] string ModName,
 	[Required] int LectID
 	//DateOnly Created,
 	//DateOnly Modified,
--- HMS_API/Program.cs
+++ HMS_API/Program.cs
@@ -1,6 +1,28 @@
+using HMS_API;
+using HMS_API.Data;
+using HMS_API.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connection String setup
+var conn = builder.Configuration.GetConnectionString("HMS");
+builder.Services.AddSqlServer<HMS_Context>(conn)
+                .AddProblemDetails()                            //services for failed requests.
+                .AddExceptionHandler<GlobalExceptionHandler>(); //handle unexpected request exceptions
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStatusCodePages();   // default response handler checking responses with status codes between 400 and 599 that do not have a body.
+app.UseExceptionHandler();  // catch exceptions, log them, and re-execute the request in an alternate pipeline.
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.MapUserEndpoints();
+app.MapModuleEndpoints();
+app.MapEnrolmentEndpoints();
+app.MapAssignmentEndpoints();
+app.MapSubmissionEndpoints();
+app.MapFeedbackEndpoints();
 
 app.Run();
