e6a1cc958254f249cd985c8ec220fcfd9a67a271
Chris Kellerman <kellerman.chri@gmail.com>
2024-09-24 21:24:42 +0200

    Add single sample Integration Test Case

---
+++ HMS_TL_System_UnitTests/InfrastructureTests.cs
@@ -0,0 +1,16 @@
+﻿namespace HMS_TL_System_UnitTests;
+
+using NUnit.Framework;
+
+public class InfrastructureTests
+{
+    [Test]
+    public async Task IsWebRespondingAsync()
+    {
+        using HttpClient client = new();
+        var text = await client.GetStringAsync("http://localhost:5157");
+
+        Assert.That(text, Is.EqualTo("Hello World!"));
+        Console.WriteLine(text);
+    }
+}
---
+++ HMS_TL_System_UnitTests/Scripts/unittest.bat
@@ -0,0 +1,2 @@
+dotnet build ..
+nunit3-console.exe ..\bin\Debug\net8.0\HMS_TL_System_UnitTests.dll
